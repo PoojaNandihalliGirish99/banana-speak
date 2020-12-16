dd2ffb18ac4a47391739a900d9401562f344f6f2
Pooja N G <PoojaNandihalliGirish99@users.noreply.github.com>
2020-12-16 22:27:59 +0530

    docs: added script file

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 // var username   = prompt("Tell us your name");
 // alert("This script works");
 // console.log("Hello, script is working");
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+
+console.log(txtInput);
+
+function clickEventHandler(){
+    console.log("clicked");
+    console.log("input", txtInput.value);
+}
+
+btnTranslate.addEventListener("click", clickEventHandler);
