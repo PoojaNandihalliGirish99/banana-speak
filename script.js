643c764f26e26607db7536fe33d8ac6dec55bd9d
Pooja N G <PoojaNandihalliGirish99@users.noreply.github.com>
2020-12-17 15:02:43 +0530

    updated script with api and url

--- script.js
+++ script.js
@@ -3,12 +3,24 @@
 // console.log("Hello, script is working");
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var output = document.querySelector("#txt-output");
 
-console.log(txtInput);
+var url = "https://api.funtranslations.com/translate/minion.json";
+function constructUrl(input) {
+    return url + "?" + "text=" + input;
+}
 
 function clickEventHandler(){
-    console.log("clicked");
-    console.log("input", txtInput.value);
+    var text = txtInput.value;
+    fetch(constructUrl(text))
+        .then(response => response.json())
+        .then(data => {
+            var translated = data.contents.translated;
+            output.innerText = translated;
+        }).catch(errorHandler);
+}
+function errorHandler(error) {
+    console.log("error : " , error);
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);
